06171e2843eb6bcacee770073f72fd14248d9f77
Elsa Rose <elsarose.binu@gmail.com>
2026-02-22 12:25:36 +0530

    Fixed main page

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ choice = st.sidebar.selectbox("Select an Option", options)
 if choice == "Home":
     st.write("Welcome to Job Portal! Use the sidebar to navigate.")
 elif choice == "Post Job":
-    import pages.1_Post_Job
+    import pages/1_Post_Job
 elif choice == "Apply Job":
-    import pages.2_Apply_Job
+    import pages/2_Apply_Job
 elif choice == "View Jobs":
-    import pages.3_View_Jobs
+    import pages/3_View_Jobs
 elif choice == "View Applications":
-    import pages.4_View_Applications
+    import pages/4_View_Applications
 elif choice == "Validate Applications":
-    import pages.5_Validate_Applications
+    import pages/5_Validate_Applications
 elif choice == "Reset Database":
-    import pages.0_Reset_Database
+    import pages/0_Reset_Database
